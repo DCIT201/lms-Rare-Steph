385a0101fdc3a10cee58da5f0f7db9ac2cbc743b
Real-Steph <saowusu225@st.ug.edu.gh>
2024-12-19 11:27:07 -0800

    done

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,38 @@
-public class Book {
+package org.example;
 
-    // This is your working class.
-    
+//A  book class, its field and functionalites.
+public class Book {
+     private String title;
+     private String author;
+     private int genre;
+
+ //A book constructor to access objects within the Book class
+     public Book(String title, String author, int genre) {
+          this.title = title;
+          this.author = author;
+          this.genre = genre;
+     }
+//This method is used to get and set the title of the book
+     public String getTitle() {
+          return title;
+     }
+     public void setTitle(String title) {
+          this.title = title;
+     }
+
+//A method that enables the user to search for a particular book
+     public void search(String search) {
+          System.out.println("The file: "+ search + " is being retrieved to from archives");
+     }
+
+//A method that allows a new book to be added
+     public void addBook(String add){
+          System.out.println("The file: "+ add + " was added to your collection successfully.");
+     }
+
+//A method that allows a book to be deleted.
+     public void removeBook(String remove, String author){
+          System.out.println("The file: "+ remove + " was removed from your collection successfully.");
+     }
 }
+//End of the book class
